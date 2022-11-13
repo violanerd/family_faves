0a8816bc5752ba1065c992ab2baa55e2950f7122
violanerd <madalyne.cross@gmail.com>
2022-11-13 11:49:23 -0600

    added recipe posting functionaliy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from crypt import methods
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 import sqlite3
 
 
@@ -12,7 +12,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
   
     return conn
-#app.secret_key = os.getenv("secret_key")
+app.secret_key = os.getenv("secret_key")
 
 
 
@@ -25,14 +25,25 @@ def index():
     conn = get_db_connection()
     recipes = conn.execute('SELECT * FROM recipes').fetchall()
     conn.close()
-    print(recipes)
+    
     return render_template("index.html", new_recipe=recipes)
     # else:
     #     # return render_template("index.html", name = "name")
     #     return render_template("index.html", name = cursor.fetchall()) this was a test.... 
 
-@app.route("/new-recipe")
+@app.route("/new-recipe", methods=["GET", "POST"])
 def new_recipe():
+    if request.method == "POST":
+        recipe_title = request.form.get("recipe-title")
+        url = request.form.get("recipe-url")
+        description = request.form.get('recipe-description')
+        name = request.form.get('user')
+        conn = get_db_connection()
+        conn.execute('INSERT INTO recipes (recipe_title, url, description, name) VALUES (?, ?, ?, ?)',
+                         (recipe_title, url, description, name))
+        conn.commit()
+        conn.close()
+        return redirect("/")
     return render_template("new-recipe.html")
 
 @app.route("/login")
