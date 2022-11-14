193b99a6f1aa0ba4b1bfbe2c2378e200082ab26d
violanerd <madalyne.cross@gmail.com>
2022-11-14 15:37:16 -0600

    working random functionality

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 from crypt import methods
 
-from flask import Flask, render_template, request, redirect, flash, url_for, session, abort
+from flask import Flask, render_template, request, redirect, flash, url_for, session
+from flask_session import Session
 import sqlite3
-from helpers import login_required
+from helpers import login_required, random
 
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 def get_db_connection():
     conn = sqlite3.connect('family_faves.db')
@@ -76,6 +81,9 @@ def login():
                     flash("Incorrect password")
                 else:
                     session["user_id"] = user["id"]
+                    print(session)
+                    for row in session:
+                        print(row)
                     return redirect("/")
             else:
                 flash("Something went wrong")
@@ -115,5 +123,16 @@ def logout():
     session.clear()
 
     return redirect("/")
+
+@app.route("/random", methods=["GET", "POST"])
+def get_random():
+    if request.method=="POST":
+        dummy_data = {'url': 'https://familystylefood.com/wheat-berries-tomato-arugula-ricotta/', 'name': 'Wheat Berry Salad with Charred Tomato', 'img': 'https://edamam-product-images.s3.amazonaws.com/web-img/241/2410460e8b1301fa2bd2992c39b1ef8f.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEOX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQDVf7v1%2BCvYetkQdvfuaufWLeSYor9lJosdgmw1AasUDwIhAIPeZJ5eklUr7yc6pyG6CJdPShTp6tM2fzX33pwAHPNGKtUECN7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMMTg3MDE3MTUwOTg2IgzThLhbXg7dOzMaVyoqqQT33aPc65fSYEKn83okk736%2FSNfJtkwViBbpplBuFrGTvyB1sUlDQwM9ApFGNwecO8eXfY%2FCtnuLrKC4M%2ByKdEknsyKccXPrDE7QZ3WNijIuPMwmQMlfhrzkTjLZaFIl2T7ZBpvb84D8sNZrHrkmwK0hL4dzJTS6k1pQg6LhL%2FUj9GSjB%2BAydH28n4G1C69D1GH6xrehVfvfzpP39euagOMCwupvpHD%2B78Qi%2FF9S1Q2apxPmLYhVyofLwgAxLsM1BDjTJ%2FA33rZoIcngwWeX9RzsF1esBpF12cd%2FDPg%2FiAdgM0Br%2F45ag%2FAlwpA5WmTx7geiplG9mHMPxrG9QCuUgpKaLwMfowpxcZ9NARbchYBHbUtYu8q%2BGPBYBEQV31dLoJW9bE9CXgBG5dxLvoPE1F9i46QzqAP%2FqYYLAqADCLEvcPmoZtXEBbRNUo%2FL3dNroKWU%2FcpfVHKBj4Pr2ug65DLFdfaIagfGsmnWpLPtXW0YCYXiR8ErWZmDW%2FYfomEuyQ%2FcCuFhfwPoLHbDqwhY1zRSftrge9gQu%2F00eD%2Ff3dXz0bxzlTxFHuuSClXDY68lODYlFQycfeBgYe8AdCBTJDKuD4xWcW5y0EuN1aWpOb04u6p4eRTh2QYywnVJeLcMqwG7X2NR%2BnlHnfzBqFclG%2F8SqPcUoXrAEYIMXpXOMQ3aKnqnYI3B2vHcZeML%2BK8P6Dr%2Frsw4BIAIQviXqUtag4sLz4QVpxXcbPsMLjHypsGOqgBOwYfwzaHvYGoFCQYeNI592eOD12IS70W15O4pQNEIuol1Isnud%2B0aqHS3IxFGJK8L1o5zAGmquzdkvgT6mafjQ8ino1kg9iikXbkIwrh2RD2U%2BZn5O9QvM2tMzfMM90RHMAXHJ9WzloIkNV5foBi4yYMdnVPkNHqpyC62fqmmBtRcHZ9XtcW%2BrKiTgTd9%2B86IOHUNJinWFqrkC8S%2FLOuP4AlPoYB5BhA&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20221114T213508Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFBCE23GPX%2F20221114%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=be352c570d5f6069a4473c4d30c9cdc694bb5f48449cd9574d631edfd67e3ef8'}
+
+        #data=random()
+        #print("made it here")
+        #print(data)
+        return render_template("randomdata.html", data=dummy_data)
+    return render_template("random.html")
 
 
--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
+import os
+import requests
 from functools import wraps
-from flask import session, redirect, url_for
-
+from flask import session, redirect, url_for, request
 
 
 def login_required(f):
@@ -11,3 +12,26 @@ def login_required(f):
             return redirect(url_for("login"))
         return f(*args, **kwargs)
     return decorated_function
+
+def random():
+    api_key = os.getenv("api_key")
+    app_id = os.getenv("application_id")
+    url = f"https://api.edamam.com/api/recipes/v2?type=public&q=&app_id={app_id}&app_key={api_key}&diet=balanced&random=true"
+    print(url)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+    except requests.RequestException:
+        return None
+    try: 
+        recipe = response.json()
+        return {
+            "url": recipe["hits"][0]['recipe']['url'],
+            "name": recipe["hits"][0]['recipe']['label'],
+            "img": recipe["hits"][0]['recipe']['image']
+
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
+
