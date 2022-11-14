de89b10d0f9140168c09c0d0fb925d4e9d584518
violanerd <madalyne.cross@gmail.com>
2022-11-13 19:22:42 -0600

    added login functionality

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 from crypt import methods
 
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, session, abort
 import sqlite3
+from helpers import login_required
 
 
 app = Flask(__name__)
@@ -10,26 +11,30 @@ app = Flask(__name__)
 def get_db_connection():
     conn = sqlite3.connect('family_faves.db')
     conn.row_factory = sqlite3.Row
-  
     return conn
+
+def get_user(username):
+    conn = get_db_connection()
+    user = conn.execute('SELECT * FROM user WHERE username = ?',
+                        (username,)).fetchone()
+    conn.close()
+    # if user is None:
+    #     flash("Something went wrong")
+    return user
+
 app.secret_key = os.getenv("secret_key")
 
 
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    # if request.method == "POST":
-        
-       
+@app.route("/")
+@login_required
+def index():      
     conn = get_db_connection()
     recipes = conn.execute('SELECT * FROM recipes').fetchall()
     conn.close()
     
     return render_template("index.html", new_recipe=recipes)
-    # else:
-    #     # return render_template("index.html", name = "name")
-    #     return render_template("index.html", name = cursor.fetchall()) this was a test.... 
 
 @app.route("/new-recipe", methods=["GET", "POST"])
 def new_recipe():
@@ -53,10 +58,38 @@ def new_recipe():
             return redirect(url_for("index"))
     return render_template("new-recipe.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    session.clear()
 
+    if request.method == "POST":
+        username = request.form.get("username")
+        if not request.form.get("username"):
+            flash("Please enter a username")
+        elif not request.form.get("password"):
+            flash("Please enter a password")
+        else: 
+            user = get_user(username)
+            if user:
+                for row in user:
+                    print(row)
+                session["user_id"] = user["id"]
+                print(session)
+                return redirect("/")
+            else:
+                flash("Something went wrong")
+    return render_template("login.html")
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+
+
+
+    return render_template("signup.html")
+@app.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect("/")
 # if __name__ == '__main__':
 #     app.run()
 
